4cd64f443f4579e43439694b9061420d208fa311
sebastian yabiku <sby2710@gmail.com>
2020-06-14 13:02:20 -0500

    Updates

---
+++ precache-manifest.a91e712ec1d82e2c3b498b086b7bcc34.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1b92681d74e53a4bfd9525b9aeb6477",
+    "url": "/carrocompra/index.html"
+  },
+  {
+    "revision": "e3ae9244b4a220e0a60a",
+    "url": "/carrocompra/static/css/2.e94183ad.chunk.css"
+  },
+  {
+    "revision": "d5ceba8358f590b8265a",
+    "url": "/carrocompra/static/css/main.2725f96e.chunk.css"
+  },
+  {
+    "revision": "e3ae9244b4a220e0a60a",
+    "url": "/carrocompra/static/js/2.e6a1e912.chunk.js"
+  },
+  {
+    "revision": "9be48472b35189e9c6b1ed6407464107",
+    "url": "/carrocompra/static/js/2.e6a1e912.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5ceba8358f590b8265a",
+    "url": "/carrocompra/static/js/main.32464a75.chunk.js"
+  },
+  {
+    "revision": "9db1ff6c1c522d1dff01",
+    "url": "/carrocompra/static/js/runtime-main.cd99a6e6.js"
+  },
+  {
+    "revision": "5a3d915dfe61cd330da08218ddd12a8c",
+    "url": "/carrocompra/static/media/arrow.5a3d915d.svg"
+  },
+  {
+    "revision": "d7caacb60761162b57c978878ada2525",
+    "url": "/carrocompra/static/media/car.d7caacb6.svg"
+  },
+  {
+    "revision": "8690c88b272d2d13e6575e2930d327b0",
+    "url": "/carrocompra/static/media/check1.8690c88b.jpg"
+  },
+  {
+    "revision": "835a1326839115c49c6ee87395e6a8fb",
+    "url": "/carrocompra/static/media/delete.835a1326.svg"
+  },
+  {
+    "revision": "23c43d0bbef4bdfa5a614cb8739656ff",
+    "url": "/carrocompra/static/media/item.23c43d0b.png"
+  },
+  {
+    "revision": "898b54e82d0bb5d4cbd3812cf6b2191d",
+    "url": "/carrocompra/static/media/niubiz.898b54e8.png"
+  },
+  {
+    "revision": "9ab0e3a3b5fdab9f79cba69d3c879659",
+    "url": "/carrocompra/static/media/user.9ab0e3a3.svg"
+  },
+  {
+    "revision": "7589ac66ab46be7906752b44b8a3681e",
+    "url": "/carrocompra/static/media/whatsapp.7589ac66.svg"
+  }
+]);
